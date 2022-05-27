8477ad842b4c67305cccaeee448f58d53a938322
sinjinu <jinu712@gmail.com>
2022-05-27 14:47:41 +0900

    Initial Commit

---
+++ Objective-C_Study/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_Study
+//
+//  Created by 신진우 on 2022/05/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
