53b865440351777fc6c7cfccdcbc3c1b0edff4bf
sinjinu <jinu712@gmail.com>
2022-09-19 16:02:42 +0900

    객체 생성 기초

--- Objective-C_Study/main.m
+++ Objective-C_Study/main.m
@@ -53,10 +53,43 @@ int main(int argc, const char * argv[]) {
         NSLog(@"======================================================================");
         NSLog(@"============================ 객체 =====================================");
         
+        //객체 생성
         NSDate *now = [NSDate date];
+        
+        //객체의 메모리 주소
         NSLog(@"The new Date lives at %p",now);
         
-        NSLog(@"형상관리ㅇㅇㅇ");
+        // %@ <- 날짜 출력 포맷, 내부의 decription 함수 호출,
+        NSLog(@"The date is %@",now);
+        
+        /**
+         메시지 보내기는 항상 대괄호, 크게 2분류
+            1. 메시지를 받는 객체의 포인터
+            2. 트리거 될 메소드의 이름
+         */
+        
+        
+        // 메시지 보내기는 함수 호출 처럼 인수를 가져도 됨.
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"%.0f seconds since the start of 1970",seconds);
+        
+        // 새로운 날짜객체 생성
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@",later);
+        
+        // id --> 어떤 오브젝티브-C 객체를 가리키는 포인터, 정확히 어떤 객체를 가리킬지 특정 하지 않고 포인터만 만들때 사용
+        id delegate;
+        
+        NSDateComponents *comps = [[NSDateComponents alloc]init];
+        [comps setYear:1993];
+        [comps setMonth:7];
+        [comps setDay:10];
+        
+        //도전과제 NSDate 객체를 2개 생성하여서 현재시각부터 생일까지 차이 구하기
+        NSCalendar *g = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        float surviveSec = [now timeIntervalSinceDate:dateOfBirth];
+        NSLog(@"생일부터 지금까지 생존한 시간 %.0f",surviveSec);
         
         
         
